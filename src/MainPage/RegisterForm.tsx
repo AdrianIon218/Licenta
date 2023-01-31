c508d7e748f17558d7d50e64a3cbc883c60c436b
Adrian <ionadrian218@yahoo.com>
2023-01-31 17:30:07 +0200

    modify the radio button in the register section

--- src/MainPage/RegisterForm.tsx
+++ src/MainPage/RegisterForm.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
+import RadioButton from "./RadioButton";
 
+enum KnowlegdeLevel {
+  BEGGINER, INTERMEDIATE, ADVANCED
+};
 
 export default function RegisterForm(){
-  
+  const [level, setLevel] = useState(KnowlegdeLevel.BEGGINER);
+
+  function submit(event:React.FormEvent<HTMLFormElement>){
+    event.preventDefault();
+    
+  }
+
   return (
     <section className="section-sign-up">
       <div className="flex-row--centered">
         <div className="box-sign-up">
           <div className="box-sign-up__form">
-            <form action="#" className="forn">
+            <form onSubmit={submit} className="forn">
               <div className="u-margin-bottom-medium u-center-text">
                 <h2 className="heading-secondary">Înregistrare curs</h2>
               </div>
@@ -24,30 +35,13 @@ export default function RegisterForm(){
               
               <div className="form__group u-margin-bottom-intermediate">
                 <h3 className="form__sub-heading">Care este nivelul tău de germană ?</h3>
+                
+                <RadioButton id="beginner" name="level" value={KnowlegdeLevel.BEGGINER} change={() => setLevel(KnowlegdeLevel.BEGGINER)} label="Începător" validate={()=>level === KnowlegdeLevel.BEGGINER} />
 
-                <div className="form__radio-group">
-                  <input type="radio" className="form__radio-input" id="beginner" name="level" />
-                  <label htmlFor="beginner" className="form__radio-label"> 
-                    <span className="form__radio-button"></span>
-                    Începător
-                  </label>
-                </div>
+                <RadioButton id="intermediate" name="level" value={KnowlegdeLevel.INTERMEDIATE} change={() => setLevel(KnowlegdeLevel.INTERMEDIATE)} label="Intermediar" validate={()=>level === KnowlegdeLevel.INTERMEDIATE} />
 
-                <div className="form__radio-group">
-                  <input type="radio" className="form__radio-input" id="intermediate" name="level"/>
-                  <label htmlFor="intermediate" className="form__radio-label"> 
-                    <span className="form__radio-button"></span>
-                    Intermediar
-                  </label>
-                </div>
-
-                <div className="form__radio-group">
-                  <input type="radio" className="form__radio-input" id="advanced" name="level"/>
-                  <label htmlFor="advanced" className="form__radio-label"> 
-                    <span className="form__radio-button"></span>
-                    Avansat
-                  </label>
-                </div>
+                <RadioButton id="advanced" name="level" value={KnowlegdeLevel.ADVANCED}
+                  change={() => setLevel(KnowlegdeLevel.ADVANCED)} label="Avansat" validate={()=>level === KnowlegdeLevel.ADVANCED} />
               </div>
               
               <div className="form__group">
