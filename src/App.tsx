e097a673129c137722d0a42645f5fea93c2cbe73
Adrian <ionadrian218@yahoo.com>
2023-02-14 23:39:48 +0200

    backdrop go back function from bg click

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 
 import Layout from './MainPage/Layout';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
-  return (<Layout> </Layout>);
+  return (<Router>
+  <Layout>
+  </Layout></Router>);
 }
 
 export default App
--- src/components/Blackdrop.tsx
+++ src/components/Blackdrop.tsx
@@ -1,4 +1,5 @@
 import {IProps} from '../CommonInterfaces';
+import {useNavigate } from 'react-router-dom';
 
 interface IBlackdropProps extends IProps{
   popupId:string,
@@ -6,7 +7,14 @@ interface IBlackdropProps extends IProps{
 }
 
 export default function Blackdrop(props:IBlackdropProps){
-  return (<section className='blackdrop' id={props.popupId}>
+  const goBack = useNavigate();
+
+  function exitBackdrop(event: React.MouseEvent<HTMLElement, MouseEvent>){
+    event.stopPropagation();
+    goBack(-1);
+  }
+
+  return (<section className='blackdrop' id={props.popupId} onClick={exitBackdrop}>
     <div className='blackdrop__content'>
       <div className='blackdrop__exit'>
         <a href={`#${props.backElementId}`} className="blackdrop__exit__btn">&times;</a>
