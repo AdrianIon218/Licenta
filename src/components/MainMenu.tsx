0eaa2978b0d0a0518556c98ff69e61375f7cf186
Adrian <ionadrian218@yahoo.com>
2023-02-16 10:04:17 +0200

    try to fix the menu position problem

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,9 +1,7 @@
 
 
 export default function MainMenu(){
-  return(<nav className="navigation">
-    <div className="navigation__btn">
+  return(<div className="navigation__btn">
       <span className="navigation__btn__icon">&nbsp;</span>
-    </div>
-  </nav>);
+    </div>);
 }
