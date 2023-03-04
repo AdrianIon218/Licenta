e3aafcd4e17d49245d0eb4fb20d6d5478539969c
Adrian <ionadrian218@yahoo.com>
2023-03-04 18:12:15 +0200

    added titles to the composition photos

--- src/components/CompositionPhoto.tsx
+++ src/components/CompositionPhoto.tsx
@@ -7,10 +7,11 @@ export interface ICompositionPhotoProps{
 
 function CompositionPhoto(props:ICompositionPhotoProps){
   return (
-    <img src={`images/${props.photo}`} alt={props.alt} 
-      className={`composition__photo ${props.class}`}
-      title={props.title? props.title:"" }  
-    />);
+    <div className="composition__photo__container">
+      <img src={`images/${props.photo}`} alt={props.alt} 
+        className={`composition__photo ${props.class}`}  />
+      <span className="composition__photo__title">{props.title}</span>
+    </div>);
 }
 
 export default CompositionPhoto;
