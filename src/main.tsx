d983cc12f1bac0c0b2ba7de99cf3df259b2906bd
Adrian <ionadrian218@yahoo.com>
2023-03-26 17:54:27 +0300

    changed the sass run , try to solve the phot problem in header

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import '../sass/index.scss';
 import TransitionContext from './components/Helpers/TransitionContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
