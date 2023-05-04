e93d569d805183cdb1dc4d6a9e5e4a14439904da
AdrianIon218 <ionadrian218@yahoo.com>
2023-05-04 13:15:46 +0300

    Updates

--- assets/StartView-e951a61f.js
+++
@@ -1,1 +0,0 @@
-import{j as a,a as e}from"./index-464a880c.js";function l(t){return a("div",{className:"flex-column--centered start-view__ctn",children:[e("h1",{className:"heading-secondary",children:t.title}),e("button",{className:"btn btn--blue",onClick:t.startClickHandler,children:"Start"})]})}export{l as default};
