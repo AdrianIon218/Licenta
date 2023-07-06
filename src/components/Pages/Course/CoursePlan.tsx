a8ab5e6623be13f5b84374cf8fee2d8aabf65aa4
AdrianIon218 <ionadrian218@yahoo.com>
2023-07-07 00:19:31 +0300

    add the last level functionality

--- src/components/Pages/Course/CoursePlan.tsx
+++ src/components/Pages/Course/CoursePlan.tsx
@@ -1,7 +1,8 @@
-import { lazy } from 'react';
+import { lazy, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Level } from '../../Helpers/constants';
 import CourseContext from '../../Features/CourseContext';
+import axios from 'axios';
 
 const NoPage = lazy(() =>import("../NoPage"));
 const CoursePanel = lazy(()=> import('./CoursePanel'));
@@ -9,9 +10,17 @@ const CoursePanel = lazy(()=> import('./CoursePanel'));
 function CoursePlan() {
   const {id:levelStr} = useParams();
   const level = [Level.A1,Level.A2,Level.B1,Level.B2,Level.C1].find((l)=>Level[l] === levelStr);
+
   if(level === undefined){
     return (<NoPage />);
   }
+
+  useEffect(()=>{
+    const userEmail = localStorage.getItem('userAccount')
+    if(userEmail && level){
+      axios.put("http://localhost:5000/user/update_last_level",{userEmail:userEmail, level: levelStr});
+    }
+  },[]);
 
   return (
     <CourseContext>
